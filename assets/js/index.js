004fabb8a61dca03815ff6e3e89309ca6ce67b56
Edson Yamble <edsonyamble2@gmail.com>
2024-03-14 12:13:35 +0700

    correction paragraph and line

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var element = document.getElementById("text-roller");
-    var text_roller = new TextRoller(element);
-    text_roller.start();
-});
+document.addEventListener('DOMContentLoaded', function () {
+	var element = document.getElementById('text-roller')
+	var text_roller = new TextRoller(element)
+	text_roller.start()
+})
